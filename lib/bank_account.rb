3834ce8311ef646b45da487c7ce0c2e0c4d1c109
Ryan K Burns <ryankburns@gmail.com>
2020-10-12 17:07:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,7 +4,7 @@ class BankAccount
 attr_reader :name
 attr_accessor :balance, :status
 
-  def initialize(name, balance = 1000, status)
+  def initialize(name, balance = 1000)
     @name = name
     @balance = balance
     @status = "open"
