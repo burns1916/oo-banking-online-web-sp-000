d98c16ab929752395a1a5614836c5f45a0990f92
Ryan K Burns <ryankburns@gmail.com>
2020-10-12 17:00:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,14 +19,14 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending" && @sender.valid? == true
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.valid? == false
-       @status = "rejected"
-       return "Transaction rejected. Please check your account balance."
-      end
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse_transfer
