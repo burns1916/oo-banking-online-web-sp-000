52c4a62878a33a2dae4c0b9729266206e49b8a41
Ryan K Burns <ryankburns@gmail.com>
2020-10-12 16:49:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
-  
+
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
