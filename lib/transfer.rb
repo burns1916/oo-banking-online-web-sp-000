13a63aed6f1aadf41eb911b3d36353266e8080ed
Ryan K Burns <ryankburns@gmail.com>
2020-10-12 17:13:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "closed"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
