2bc9c95babb047995d6803de12c01cc7b55a239e
Ryan K Burns <ryankburns@gmail.com>
2020-10-12 17:08:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status != "closed"
+    if @sender.balance > @amount && @status != "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
